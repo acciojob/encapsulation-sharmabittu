b14b8e26cd16b3d13d7f3abef874db3c30950a8e
Bittu Sharma <bittubaudh5@gmail.com>
2023-10-12 11:36:55 +0530

    Solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+
+  public static void main(String args[]){
+    RWOnly o = new RWOnly();
+    String s = "Ok Gandu.";
+    o.setMember(s);
+    String name = o.getMember();
+
+    System.out.println(name);
+  }
+
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public String getMember() {
+        return name;
+    }
+
+    public void setMember(String name) {
+        this.name = name;
+    }
 }
